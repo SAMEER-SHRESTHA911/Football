d2d349b94a38cd4cca2923dba5060bcb130c5cce
rajinkhatri <61622498+rajinkhatri@users.noreply.github.com>
2022-08-12 21:31:51 +0545

    new images, new ground, updated boundary

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pygame
 pygame.init()
 
 # creating the window
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((800,545))
 
 # changing the title and logo
 pygame.display.set_caption("Free Kick")
@@ -13,13 +13,17 @@ icon = pygame.image.load('icon.png')
 pygame.display.set_icon(icon)
 
 # changing the background
-back_ground = pygame.image.load('ground_800x605.jpg')
+back_ground = pygame.image.load('new_800x545.jpg')
 
 # creating the player
-playerImg = pygame.image.load('ball.png')
+playerImg = pygame.image.load('player.png')
 playerX = 370
 playerY = 480
 playerX_Change = 0
+
+# creating the goalpost
+goalpostImg = pygame.image.load('post.png')
+
 def player(x,y):
     screen.blit(playerImg, (x,y))
 
@@ -30,6 +34,7 @@ while game_time:
 
     screen.fill((0, 0, 0))
     screen.blit(back_ground, (0, 0))
+    screen.blit(goalpostImg,(370,0))
 
     for events in pygame.event.get():
         if events.type == pygame.QUIT:
@@ -49,10 +54,10 @@ while game_time:
 
         # adding the boundary for player
     playerX += playerX_Change
-    if playerX <= 0:
-        playerX = 0
-    elif playerX >= 768:
-        playerX = 768
+    if playerX <= 25:
+        playerX = 25
+    elif playerX >= 715:
+        playerX = 715
 
     player(playerX, playerY)
     pygame.display.update()
