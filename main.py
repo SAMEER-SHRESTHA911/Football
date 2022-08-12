20e7a28b726da2994ef1aa19dbe58c3fea2a4ec4
SAMEER SHRESTHA <Nero0293@outlook.com>
2022-08-12 20:59:53 +0545

    Minor changes

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pygame
 pygame.init()
 
 # creating the window
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((800,605))
 
 # changing the title and logo
 pygame.display.set_caption("Free Kick")
@@ -18,18 +18,33 @@ back_ground = pygame.image.load('ground_800x605.jpg')
 
 game_time = True
 
+playerX = 370
+playerY = 480
+
+
+ball_vel = 5
+radius = 10
+ballX = 350
+ballY = 400 
 
 # game loop
 while game_time:
 
     screen.fill((0,0,0))
     screen.blit(back_ground,(0,0))
+    pygame.draw.circle(screen,(255,255,255),(ballX,ballY),radius)
 
     for events in pygame.event.get():
         if events.type == pygame.QUIT:
             game_time = False
 
-
+#changing position of shooter 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        playerX -= 0.3
+    if keys[pygame.K_RIGHT]:
+        playerX += 0.3
 
 
     pygame.display.update()
+pygame.quit()
